b3ad657004f515f7070e6ba47993ef4732aaf790
chaumont-arch <chaumont@ualberta.ca>
2021-12-29 02:41:31 -0700

    add an exit

--- athena.py
+++ athena.py
@@ -1,13 +1,25 @@
 import wikipedia
+import re
+
+quit_strings = ["quit","bye.?"]
 
 def get_input():
     #I'm sure this will be more complicated later.
     prompt = "Enter prompt: "
     return input(prompt)
 
+def regex_bool(string,regex_array):
+    for regex_exp in regex_array:
+        if len(re.findall(regex_exp,string)) > 0:
+            return True
+    return False
+
 def parse_input(user_input):
     lower_input = user_input.lower()
-    if lower_input == "quit":
+   # if lower_input == "quit":
+    #    return False
+    if regex_bool(lower_input,quit_strings):
+        print("bye!")
         return False
     else:
         print("unrecognized input")
